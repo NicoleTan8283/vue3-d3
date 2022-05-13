9821e47a18239cfb94f9eb26a54cb29ad2dcf8ba
zhaojianzz <1071438836@qq.com>
2022-05-13 10:54:02 +0800

    add more config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,65 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from "rollup-plugin-visualizer";
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import checker from "vite-plugin-checker";
+import viteCompression from "vite-plugin-compression";
+import legacy from "@vitejs/plugin-legacy";
 import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
+const BUILD_VSL = process.env.BUILD_VSL !== undefined;
+const filter = /\.(js|css|json|txt|html|ico|svg|ttf|eot|woff)(\?.*)?$/i;
 export default defineConfig({
   base: './',
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
     Components({
+      dts: resolve('./components.d.ts'),
       resolvers: [ElementPlusResolver()]
     }),
     createSvgIconsPlugin({
       iconDirs: [resolve(process.cwd(), 'src/icons')],
       symbolId: 'icon-[dir]-[name]',
     }),
-    WindiCSS()
+    WindiCSS(),
+    legacy({
+      ignoreBrowserslistConfig: true,
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+    BUILD_VSL && visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      open: true,
+    }),
+    viteCompression({
+      filter,
+      threshold: 10240
+    }),
+    chunkSplitPlugin(),
+    {
+      ...checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{vue,js,mjs,cjs,jsx,ts,tsx}"',
+          dev: {
+            logLevel: ["error"],
+          },
+        },
+      }),
+      apply: "serve",
+    },
   ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
   },
+  build: {
+    target: ["es2015"]
+  }
 })
