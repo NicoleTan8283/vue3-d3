9361e6c47878aab1a9954bd109bdf30e30ddcd3e
zhaojianzz <1071438836@qq.com>
2022-06-20 14:38:45 +0800

    去掉imago

--- src/views/vto/Index.vue
+++ src/views/vto/Index.vue
@@ -6,11 +6,6 @@
 
 <script setup lang="ts">
 import vtoComponentsVue from '../../components/vtoComponents.vue';
-import { callWasm, callWasmAsync, getThreadMode } from 'zzzj'
-console.log(callWasmAsync((f: any) => {
-  console.log('f :>> ', f);
-}));
-console.log('callWasmAsync :>> ', callWasmAsync);
 
 </script>
 
